a776aeb7fe8799d84c705770761f49d141e55da3
borisstevanovic96 <131660123+borisstevanovic96@users.noreply.github.com>
2024-08-19 12:05:31 +0200

    Update Register.js

--- ui/src/components/Register.js
+++ ui/src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from "axios"
 import { API_URL } from '../utils'; 
-
+import '../register.css'; // Import the CSS file
 function Register() {
   const [formData, setFormData] = useState({
     username: '',
